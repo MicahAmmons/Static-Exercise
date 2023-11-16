544bd0db5686baa16abed7b8d1985fd2611e3ae8
Micah Ammons <micahammons@gmail.com>
2023-11-16 12:16:36 -0500

    Completed work

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double int1 = TempConverter.CelsiusToFahrenheit(20);
+            double int2 = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"20 Celcius is {int1} in Fahrenheri and 68 Fahrenhei is {int2} in Celcius.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempF)
+        {
+            return (tempF - 32) * (5.0 / 9.0);
+        }
+        public static double CelsiusToFahrenheit(double tempC)
+        {
+            return tempC * (9.0 / 5.0) + 32;
+        }
+    }
+}
